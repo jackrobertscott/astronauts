31dc9b400144c0c2cb2009027b02ddc669990bcc
Jack Scott <jack.rob.scott@gmail.com>
2019-02-28 17:58:41 +0800

    began fleshing out the functionality needed for requests

--- packages/astronauts/src/modules/Fetch.ts
+++ packages/astronauts/src/modules/Fetch.ts
@@ -1,6 +1,62 @@
+import config from '../config';
+
+export interface IFetchOptions {
+  id: string;
+}
+
+export interface IFetchSendOptions {
+  action: string;
+  type: string;
+}
+
 /**
  * This module manages any fetch requests that need to
  * be sent to the server. It is a simple interface
  * over the top of the native fetch library.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
  */
-export default class Fetch {}
+export default class Fetch {
+  private id: string;
+
+  constructor({ id }: IFetchOptions) {
+    this.id = id;
+  }
+
+  /**
+   * Send json data to the server.
+   *
+   */
+  public data(data: any, { action }: { action: string }) {
+    return this.core(JSON.stringify(data), {
+      action,
+      type: 'application/json',
+    });
+  }
+
+  /**
+   * Upload files to the server.
+   */
+  public files(files: FormData, { action }: { action: string }) {
+    return this.core(files, {
+      action,
+      type: 'application/x-www-form-urlencoded',
+    });
+  }
+
+  /**
+   * Send anything to the server.
+   */
+  public core(data: any, { action, type }: IFetchSendOptions) {
+    const headers = new Headers();
+    headers.set('Content-Type', type);
+    headers.set('Bearer', this.id);
+    const options = {
+      method: 'post',
+      headers,
+      body: data,
+    };
+    const resolve = (response: Response) => response.json();
+    return fetch(`${config.endpoint}/${action}`, options).then(resolve);
+  }
+}
--- packages/astronauts/src/modules/Store.ts
+++ packages/astronauts/src/modules/Store.ts
@@ -1,7 +1,33 @@
+import Collection from './Collection';
+
+export interface IStoreOptions {
+  id: string;
+}
+
 /**
  * This module is the controller of data and manages
  * the fetch and update requests that must occur
  * as the data changes on both the client and the
  * server.
  */
-export default class Store {}
+export default class Store {
+  private id: string;
+
+  constructor({ id }: IStoreOptions) {
+    this.id = id;
+  }
+
+  public single(domain: string) {
+    return new Collection({
+      domain,
+      type: 'object',
+    });
+  }
+
+  public multiple(domain: string) {
+    return new Collection({
+      domain,
+      type: 'array',
+    });
+  }
+}
