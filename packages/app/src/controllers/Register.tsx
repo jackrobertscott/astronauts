5ffa17b2ce18caf43b904b6e8b342edd0eec0161
Jack Scott <jack.rob.scott@gmail.com>
2019-03-18 12:40:52 +0800

    register user now works

--- packages/app/src/controllers/Register.tsx
+++ packages/app/src/controllers/Register.tsx
@@ -1,40 +1,39 @@
-import React, { useEffect, FunctionComponent } from 'react';
-import {
-  useConnection,
-  Frame,
-  useAddress,
-  useComplex,
-  useString,
-} from 'nuggets';
+import React, { FunctionComponent } from 'react';
+import { useConnection, useAddress, useComplex, useString } from 'nuggets';
 import gql from 'graphql-tag';
 import { Input } from '../components/Input';
-import { queryConnection } from '../services/queryConnection';
 import { Main } from '../components/Main';
 import { Button } from '../components/Button';
 import { Seperator } from '../components/Seperator';
+import { mutationConnection } from '../services/mutationConnection';
 
-export const GetUser = gql`
-  query GetUser {
-    projectsAll {
-      id
-      name
+export const RegisterUser = gql`
+  mutation RegisterUser($credentials: UserCredentialsInput!) {
+    usersRegister(credentials: $credentials) {
+      token
+      user {
+        id
+      }
     }
   }
 `;
 
 export interface IGetUser {
-  projectsAll: Array<{
-    id: string;
-    name: string;
-  }>;
+  usersRegister: {
+    token: string;
+    user: {
+      id: string;
+    };
+  };
 }
 
 export const Register: FunctionComponent<{}> = () => {
   const address = useAddress();
   const actionRegister = useConnection<IGetUser>({
-    connection: queryConnection({ action: GetUser }),
+    connection: mutationConnection({ action: RegisterUser }),
   });
   const credentials = useComplex();
+  const name = useString(credentials.operate('name'));
   const username = useString(credentials.operate('username'));
   const password = useString(credentials.operate('password'));
   const email = useString(credentials.operate('email'));
@@ -47,6 +46,7 @@ export const Register: FunctionComponent<{}> = () => {
   };
   return (
     <Main>
+      <Input {...name} label="Name" placeholder="E.g. Jack Scott" />
       <Input {...username} label="Username" placeholder="E.g. jack" />
       <Input {...password} label="Password" placeholder="**********" />
       <Input {...email} label="Email" placeholder="E.g. jack@example.com" />
